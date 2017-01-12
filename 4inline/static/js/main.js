a6055f6f9ebd701e294e233d021c02d06530be34
Ruben Torres <rubentdlh@gmail.com>
2017-01-12 14:56:19 -0300

    Add preview and board mask

--- 4inline/static/js/main.js
+++ 4inline/static/js/main.js
@@ -1,5 +1,31 @@
 $(document).ready( function() {
 
-  console.log('ready');
+  $('.cell').mouseover(function(){
+    var x = parseInt($(this).attr('id').split('_')[1]);
+    if($('input#state').val() != 0){
+      return;
+    }
+    $('#preview').show();
+    var turn = $('input#turn').val();
+    if(turn == 1){
+      $('#preview').addClass('yellow');
+    }else{
+      $('#preview').addClass('red');
+    }
+    $('#preview').css('left', 46 * x);
+  }).mouseout(function(){
+    $('#preview').hide();
+  });
+
+
+  $('.cell').click(function(){
+    var x = parseInt($(this).attr('id').split('_')[1]);
+    play(x);
+  });
 
 });
+
+function play(x){
+  $('input#col').val(x);
+  $('#play').submit();
+}

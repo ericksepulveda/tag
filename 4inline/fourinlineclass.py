2c5fbe94e57a0db350e3002425af17bb656d4bae
Ruben Torres <rubentdlh@gmail.com>
2017-01-12 11:49:44 -0300

    Constructor and some methods

--- 4inline/fourinlineclass.py
+++ 4inline/fourinlineclass.py
@@ -1,3 +1,14 @@
 class FourInLine:
 	def __init__(self):
-		self.board = []
+		self._board = []
+		self._turn = 1
+		self._state = 0
+
+	def getBoard(self):
+		return self._board
+
+	def getTurn(self):
+		return self._turn
+
+	def getState(self):
+		return self._state

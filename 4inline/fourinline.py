d60ab549088f907eeeababa549527f84d7d74770
Ruben Torres <rubentdlh@gmail.com>
2017-01-12 11:11:38 -0300

    Basic board UI

--- 4inline/fourinline.py
+++ 4inline/fourinline.py
@@ -1,5 +1,5 @@
 # define simple flask app
-from flask import Flask,render_template, request
+from flask import Flask, render_template, send_from_directory, request
 from fourinlineclass import FourInLine
 app = Flask(__name__)
 
@@ -15,6 +15,14 @@ def home():
 
 @app.route('/move', methods=["POST"])
 def move(): pass
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
 
 if __name__ == "__main__":
     app.run()
